acf0a8a26b58acad3cb78ef3c6cc87a876d2c1c9
TransRe-2002 <yuanxiaojian2002@gmail.com>
2022-06-16 15:57:00 +0800

    null

--- src/Express.java
+++ src/Express.java
@@ -6,4 +6,66 @@ public class Express extends DBDao
     private int Crno;
     private int ECno;
     private String Address;
+
+    public int getEno()
+    {
+        return Eno;
+    }
+
+    public int getTrno()
+    {
+        return Trno;
+    }
+
+    public int getCuno()
+    {
+        return Cuno;
+    }
+
+    public int getCrno()
+    {
+        return Crno;
+    }
+
+    public int getECno()
+    {
+        return ECno;
+    }
+
+    public String getAddress()
+    {
+        return Address;
+    }
+
+    public void setEno(int Eno)
+    {
+        this.Eno = Eno;
+    }
+
+    public void setTrno(int Trno)
+    {
+        this.Trno = Trno;
+    }
+
+    public void setCuno(int Cuno)
+    {
+        this.Cuno = Cuno;
+    }
+
+    public void setCrno(int Crno)
+    {
+        this.Crno = Crno;
+    }
+
+    public void setECno(int ECno)
+    {
+        this.ECno = ECno;
+    }
+
+    public void setAddress(String Address)
+    {
+        this.Address = Address;
+    }
+
+
 }

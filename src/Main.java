cbc85ac76058b8bbf62cb4c7de80305e8c481df8
TransRe-2002 <yuanxiaojian2002@gmail.com>
2022-07-26 23:40:48 +0800

    null

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,8 @@ import java.sql.SQLException;
 
 public class Main
 {
-    public static void main(String[] args) {
-        try
-        {
-            var pS = DBDao.getConnection().prepareStatement("SELECT COUNT(*) FROM Commodity WHERE Cono = ?");
-            pS.setInt(1,1);
-            var result = pS.executeQuery();
-            System.out.println(result.next());
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
+    public static void main(String[] args)
+    {
+        new Console().run();
     }
 }
